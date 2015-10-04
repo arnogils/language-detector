4a5f2ffff23ccf0251f48b71731942816dfb0a4c
Arno Gils <arnogils@gmail.com>
2015-10-04 15:27:22 +0200

    Initial commit

---
+++ language.py
@@ -0,0 +1,37 @@
+"""Rough representation of a language model"""
+
+from io import open
+
+
+class Language(object):
+
+    def __init__(self, language, keywords_file):
+        self.name = language
+        self.keywords = keywords_file
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, language):
+        if not language.isalpha():
+            raise ValueError('Invalid language name')
+
+        if not isinstance(language, str):
+            raise ValueError('Language must be string')
+
+        if not language[0].isupper():
+            raise ValueError('Language must start with a capital')
+
+        self.__name = language
+
+    @property
+    def keywords(self):
+        return self.__keywords
+
+    @keywords.setter
+    def keywords(self, keywords_file):
+        """Parses language specific keywords to a usable set"""
+        with open(keywords_file, mode='rt', encoding='utf-8') as f:
+            self.__keywords = set(f.read().split('\n'))
---
+++ language_detector.py
@@ -0,0 +1,35 @@
+"""Simple implementation of a language detector"""
+
+from language import Language
+
+
+class LanguageDetector:
+
+    def __init__(self):
+        self._languages = [Language('English', 'language_files/en.txt'),
+                           Language('Dutch', 'language_files/nl.txt'),
+                           Language('German', 'language_files/de.txt')]
+
+    def guess_language(self, message):
+        """Rough implementation of a language guessing algorithm.
+
+        Args:
+            Message: a message in string format.
+
+        Returns:
+            A string containing the name of the detected language.
+        """
+        matches = {}
+        for language in self._languages:
+            result = set(message.lower().split()) & message.keywords
+            if result:
+                matches[language.name] = result
+        if not matches:
+            return 'Language not detected'
+        return matches
+
+    def guess_from_string(self):
+        raise NotImplementedError
+
+    def guess_from_files(self):
+        raise NotImplementedError
