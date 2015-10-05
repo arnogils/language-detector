1dbb6008b2a0e9232b72680f05e96ca5617de86a
Arno Gils <arnogils@gmail.com>
2015-10-05 16:24:14 +0200

    Basic language tests

--- tests/language_tests.py
+++ tests/language_tests.py
@@ -4,8 +4,11 @@ from language import Language
 
 class LanguageTests(unittest.TestCase):
 
-    def setUp(self):
-        raise NotImplementedError
+    def test_needs_capital(self):
+        self.assertRaises(ValueError, Language, 'dutch', 'language_files/nl.txt')
 
-    def tearDown(self):
-        raise NotImplementedError
+    def test_name_must_be_string(self):
+        self.assertRaises(ValueError, Language, '12345', 'language_files/nl.txt')
+
+    def test_invalid_language_file(self):
+        self.assertRaises(IOError, Language, 'Dutch', '')
