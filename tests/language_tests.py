be9c81bbded7e6c1cffbae4dd1c36a4f931d1fa8
Arno Gils <arnogils@gmail.com>
2015-10-05 16:51:18 +0200

    Basic language tests

--- tests/language_tests.py
+++ tests/language_tests.py
@@ -5,16 +5,25 @@ from language import Language
 class LanguageTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.language = Language('Dutch', '../language_files/nl.txt')
 
     def test_needs_capital(self):
-        self.assertRaises(ValueError, Language, 'dutch', 'language_files/nl.txt')
+        self.assertRaises(ValueError, Language, 'dutch', '../language_files/nl.txt')
 
     def test_name_must_be_string(self):
-        self.assertRaises(ValueError, Language, '12345', 'language_files/nl.txt')
+        self.assertRaises(ValueError, Language, '12345', '../language_files/nl.txt')
 
     def test_invalid_language_file(self):
         self.assertRaises(IOError, Language, 'Dutch', '')
 
+    def test_valid_name(self):
+        self.assertEqual('Dutch', self.language.name)
+
+    def test_file_to_set(self):
+        self.assertIsInstance(self.language.keywords, set)
+
+    def test_keyword_conversion_not_empty(self):
+        self.assertGreater(len(self.language.keywords), 0)
+
     def tearDown(self):
-        pass
+        print "Finished test {0}".format(self._testMethodName)
